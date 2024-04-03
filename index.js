38f7b4f694998f9420c2179c9a54862840761ccd
dhubmann <daniel.hubmann@gmx.at>
2024-04-03 15:36:08 +0200

    add .env

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express");
+require("dotenv").config();
 const mongoose = require("mongoose");
 const app = express();
+const db = process.env.DATABASE_URL;
+const port = process.env.PORT;
 
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
 });
 
 mongoose
-  .connect(
-    "mongodb+srv://danielhubmann:OOtcj6jNJFskHU5o@backenddb.6oykvxj.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
-  )
+  .connect(db)
   .then(() => {
     console.log("Connected to database!");
     app.listen(3000, () => {
-      console.log("Server running on port 3000...");
+      console.log(`Server running on port ${port}...`);
     });
   })
   .catch(() => console.log("Connection failed!"));
