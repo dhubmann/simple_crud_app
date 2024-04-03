0aba188c18b0bfe0dff41a6cab708004402d8547
dhubmann <daniel.hubmann@gmx.at>
2024-04-03 15:08:40 +0200

    setup db connection

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000...");
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello from Node API");
-});
+mongoose
+  .connect(
+    "mongodb+srv://danielhubmann:OOtcj6jNJFskHU5o@backenddb.6oykvxj.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server running on port 3000...");
+    });
+  })
+  .catch(() => console.log("Connection failed!"));
