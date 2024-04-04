86a266c97b6dacf5f9ed99ab4fc2ce39ff3129d9
dhubmann <daniel.hubmann@gmx.at>
2024-04-04 19:25:13 +0200

    add api functions

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
 const db = process.env.DATABASE_URL;
 const port = process.env.PORT;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
+});
+
+// get all products
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// get one product by id
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// add a new product
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
